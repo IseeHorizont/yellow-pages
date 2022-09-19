518c56f855f167b36b542f63e010c4dc7e041ba4
Evgeny <angst_eam@mail.ru>
2022-09-19 14:58:30 +0300

    add readme

--- src/main/java/com/example/yellowpages/YellowPagesApplication.java
+++ src/main/java/com/example/yellowpages/YellowPagesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class YellowPagesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(YellowPagesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(YellowPagesApplication.class, args);
+    }
 
 }
